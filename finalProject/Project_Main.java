42b4b467861ca98017e270c2bca1eb2c6da07154
maizehydra64 <maizehydra64@gmail.com>
2024-05-04 14:09:26 -0400

    Added the menu and updated the date

--- finalProject/Project_Main.java
+++ finalProject/Project_Main.java
@@ -2,12 +2,12 @@
  * 
  */
 package finalProject;
-
 /**
  * @author 
  * @author Isaac Fitzwater
- * Project Due: 4/29/2024
+ * Project Due: 5/06/2024
  */
+
 public class Project_Main {
 
 	/**
@@ -16,6 +16,22 @@ public class Project_Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+	}
+	
+	// Selection menu for the program
+	public static void menu() {
+		System.out.println("Algorithmic Numbers Menu:");
+		System.out.println("1. Even Numbers");
+		System.out.println("2. Buzz Numbers");
+		System.out.println("3. Palindrome Numbers");
+		System.out.println("4. Prime Numbers");
+		System.out.println("5. Gapful Numbers");
+		System.out.println("6. Jumping Numbers");
+		System.out.println("7. Perfect Square Numbers");
+		System.out.println("8. Harmonic Numbers");
+		System.out.println("9. Placeholder");
+		System.out.println("0. Exit");
+		System.out.println("Please enter the number of your choice: ");
 	}
 
 }
