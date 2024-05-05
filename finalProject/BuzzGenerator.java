faba07c61bb7f20e0e9cff556d4e51a80ac7583c
claypoops <168984682+claypoops@users.noreply.github.com>
2024-05-05 13:13:09 -0400

    Update BuzzGenerator.java
    made generator for buzz numbers

--- finalProject/BuzzGenerator.java
+++ finalProject/BuzzGenerator.java
@@ -1,1 +1,43 @@
+package FinalStuff;
+
+public class BuzzGenerator {
+	short[] numbers;
+	
+	// constructor
+	public BuzzGenerator(int length) {
+		numbers = new short[length];
+		generate();
+	}
+	
+	public void generate() {
+        int current = 1;
+        int found = 0;
+        while (found < numbers.length) {
+            if (current % 7 == 0 || current % 10 == 7) {
+                numbers[found++] = (short) current;
+            }
+            current++;
+        }
+    }
+	
+	// flips the list
+		public void flip() {
+			int len = numbers.length;
+			short[] tempList = new short[len];
+			for(int i = 0;i<len;i++) {
+				tempList[i] = numbers[len-(i+1)];
+			}
+			numbers = tempList;
+		}
+		
+		// prints the list
+		public void print() {
+			for(int i = 0; i < numbers.length;i++) {
+				System.out.print(numbers[i]);
+				if(i!=numbers.length-1) {
+					System.out.print(", ");
+				}
+			}
+		}
+}
 
