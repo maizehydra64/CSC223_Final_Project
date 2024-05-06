618a08e6d02d31369e5a0ac3f78ba802cce37931
claypoops <168984682+claypoops@users.noreply.github.com>
2024-05-06 00:33:02 -0400

    Update BuzzGenerator.java

--- finalProject/BuzzGenerator.java
+++ finalProject/BuzzGenerator.java
@@ -1,43 +1,37 @@
 package finalProject;
 
+import java.util.Stack;
+
 public class BuzzGenerator {
-	short[] numbers;
-	
+
+	//generic stack
+	Stack<Short> numbers;
+
 	// constructor
 	public BuzzGenerator(int length) {
-		numbers = new short[length];
-		generate();
+		numbers = new Stack<>();
+		generate(length);
 	}
-	
-	public void generate() {
-        int current = 1;
-        int found = 0;
-        while (found < numbers.length) {
-            if (current % 7 == 0 || current % 10 == 7) {
-                numbers[found++] = (short) current;
-            }
-            current++;
-        }
-    }
-	
-	// flips the list
-		public void flip() {
-			int len = numbers.length;
-			short[] tempList = new short[len];
-			for(int i = 0;i<len;i++) {
-				tempList[i] = numbers[len-(i+1)];
+	//Uses math to cycle through what numbers best fit this Algorithm
+	public void generate(int length) {
+		short current = 1;
+		int found = 0;
+		while (found < length) {
+			if (current % 7 == 0 || current % 10 == 7) {
+				numbers.push(current);
+				found++;
 			}
-			numbers = tempList;
+			current++;
 		}
-		
-		// prints the list
-		public void print() {
-			for(int i = 0; i < numbers.length;i++) {
-				System.out.print(numbers[i]);
-				if(i!=numbers.length-1) {
-					System.out.print(", ");
-				}
+	}
+	// prints the list
+	public void print() {
+		while(!numbers.isEmpty()) {
+			System.out.print(numbers.pop());
+			if(!numbers.isEmpty()) {
+				System.out.print(", ");
 			}
 		}
+		System.out.println();
+	}
 }
-
